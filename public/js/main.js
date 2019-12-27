094555de8a7f60748b699b0f54be5e8af355a1ca
Levente Proksa <proksalevente@gmail.com>
2019-12-27 22:43:25 +0100

    started building site

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+(() => {
+
+	function elById(id) {
+		return document.getElementById(id);
+	}
+
+	const usernameText = elById('username');
+	const paswordText = elById('password');
+	const loginBtn = elById('login_btn');
+
+
+
+});
