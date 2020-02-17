e9573e17fd3697eb047a8df099c03288b2a657bd
Levente Proksa <proksalevente@gmail.com>
2020-02-17 13:37:00 +0100

    starting to fix things up

--- server.js
+++ server.js
@@ -9,73 +9,87 @@ const pubPath = __dirname + '/public/';
 const port = 3000;
 
 const con = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'test'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test'
 });
 
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: 'very secret secret',
+    secure: false
+  })
+);
 
 app.use(express.static(pubPath));
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 app.get('/', (req, res) => {
-	res.sendFile(pubPath + 'index.html');
+  if (req.session.loggedin) res.sendFile(pubpath + 'index_loggedin.html');
+  else res.sendFile(pubPath + 'index.html');
 });
 
 app.get('/login', (req, res) => {
-	res.sendFile(pubPath + 'login.html');
+  res.sendFile(pubPath + 'login.html');
 });
 
 app.get('/register', (req, res) => {
-	res.sendFile(pubPath + 'register.html');
+  res.sendFile(pubPath + 'register.html');
 });
 app.post('/api/reg', (req, res) => {
-	let email = req.body.email;
-	let username = req.body.username;
-	let password = req.body.password;
-	let password2 = req.body.password2;
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
+  let password2 = req.body.password2;
 
-	if (email && username && password === password2) {
-		if (con.query('SELECT * FROM users WHERE username = ?', [username], (err, results, fields) => {
-				if (results.length > 0) res.send('This username is already taken!');
-				else {
-					con.query('INSERT INTO users (email, username, password) values (?, ?, ?)', [email, username, password], (error) => {
-						if (error)
-							throw error;
-					});
-					req.session.loggedin = true;
-					req.session.username = username;
-					res.redirect('/')
-				}
-			}));
-	} else res.send('Wrong information input!');
+  if (email && username && password === password2) {
+    con.query(
+      'SELECT * FROM users WHERE username = ?',
+      [username],
+      (err, results, fields) => {
+        if (results.length > 0) res.send('This username is already taken!');
+        else {
+          con.query(
+            'INSERT INTO users (email, username, password) values (?, ?, ?)',
+            [email, username, password],
+            error => {
+              if (error) throw error;
+            }
+          );
+          req.session.loggedin = true;
+          req.session.username = username;
+          res.sendFile(pubpath + '/index_loggedin.html');
+        }
+      }
+    );
+  } else res.send('Wrong information input!');
 });
 app.post('/api/auth', (req, res) => {
-	let username = req.body.username;
-	let password = req.body.password;
-	if (username && password) {
-		con.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (error, results, fields) => {
-			if (results.length > 0) {
-				req.session.loggedin = true;
-				req.session.username = username;
-				res.redirect('/')
-			} else res.send('Wrong username and/or password!');
-		})
-	}
+  let username = req.body.username;
+  let password = req.body.password;
+  if (username && password) {
+    con.query(
+      'SELECT * FROM users WHERE username = ? AND password = ?',
+      [username, password],
+      (error, results, fields) => {
+        if (results.length > 0) {
+          req.session.loggedin = true;
+          req.session.username = username;
+          res.redirect('/');
+        } else res.send('Wrong username and/or password!');
+      }
+    );
+  }
 });
 
-
 app.listen(port, () => {
-	console.log('Example app listening on port ' + port + '!');
+  console.log('Example app listening on port ' + port + '!');
 });
